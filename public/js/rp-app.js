30fe49e2c3de800d838de2cc466c526141ceff8d
Adam Powers <apowers@ato.ms>
2016-06-05 16:34:42 -0700

    sending real data, setting RP value, encoding ArrayBuffers before sending to server

--- public/js/rp-app.js
+++ public/js/rp-app.js
@@ -77,8 +77,9 @@ function fidoRegister() {
 
     // TODO: get registration request from server
     var accountInfo = {
-        rpDisplayName: 'Facebook', // human readable name of service
-        userDisplayName: document.getElementById("username").value // name of person registering
+        rpDisplayName: 'WebAuth Demo App', // human readable name of service
+        displayName: document.getElementById("username").value // name of person registering
+
     };
 
     var cryptoParameters = [{
@@ -90,30 +91,30 @@ function fidoRegister() {
     getRegisterChallenge()
         .then(function(challenge) {
             console.log("Doing makeCredential");
-            // return window.webauthn.makeCredential(accountInfo, cryptoParameters);
+            return window.webauthn.makeCredential(accountInfo, cryptoParameters);
 
-            // dummy credential
-            var cred = {
-                credential: {
-                    type: 'ScopedCred',
-                    id: '8DD7414D-EE43-474C-A05D-FDDB828B663B'
-                },
-                publicKey: {
-                    kty: 'RSA',
-                    alg: 'RS256',
-                    ext: false,
-                    n: 'lMR4XoxRiY5kptgHhh1XLKnezHC2EWPIImlHS-iUMSKVH32WWUKfEoY5Al_exPtcVuUfcNGtMoysAN65PZzcMKXaQ-2a8AebKwe8qQGBc4yY0EkP99Sgb80rAf1S7s-JRNVtNTRb4qrXVCMxZHu3ubjsdeybMI-fFKzYg9IV6DPotJyx1OpNSdibSwWKDTc5YzGfoOG3vA-1ae9oFOh5ZolhHnr5UkodFKUaxOOHfPrAB0MVT5Y5Stvo_Z_1qFDOLyOWdhxxzl2at3K9tyQC8kgJCNKYsq7-EFzvA9Q90PC6SxGATQoICKn2vCNMBqVHLlTydBmP7-8MoMxefM277w',
-                    e: 'AQAB'
-                },
-                attestation: null
-            };
-            console.log("makeCredential done, returned:");
-            console.log(cred);
-            cred.username = accountInfo.userDisplayName;
-            return cred;
+            // // dummy credential
+            // var cred = {
+            //     credential: {
+            //         type: 'ScopedCred',
+            //         id: '8DD7414D-EE43-474C-A05D-FDDB828B663B'
+            //     },
+            //     publicKey: {
+            //         kty: 'RSA',
+            //         alg: 'RS256',
+            //         ext: false,
+            //         n: 'lMR4XoxRiY5kptgHhh1XLKnezHC2EWPIImlHS-iUMSKVH32WWUKfEoY5Al_exPtcVuUfcNGtMoysAN65PZzcMKXaQ-2a8AebKwe8qQGBc4yY0EkP99Sgb80rAf1S7s-JRNVtNTRb4qrXVCMxZHu3ubjsdeybMI-fFKzYg9IV6DPotJyx1OpNSdibSwWKDTc5YzGfoOG3vA-1ae9oFOh5ZolhHnr5UkodFKUaxOOHfPrAB0MVT5Y5Stvo_Z_1qFDOLyOWdhxxzl2at3K9tyQC8kgJCNKYsq7-EFzvA9Q90PC6SxGATQoICKn2vCNMBqVHLlTydBmP7-8MoMxefM277w',
+            //         e: 'AQAB'
+            //     },
+            //     attestation: null
+            // };
+            // console.log("makeCredential done, returned:");
+            // console.log(cred);
+            // return cred;
         })
         .then(function(cred) {
             console.log("Doing sendRegisterResponse");
+            cred.username = accountInfo.displayName;
             return sendRegisterResponse(cred);
         })
         .catch(function(err) {
@@ -165,9 +166,14 @@ function sendLoginResponse(assn) {
         xhr.open("POST", fidoServerLoginEndpoint, true);
         xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
         // xhr.send(JSON.stringify(req));
-        var a = JSON.stringify(assn);
-        console.log("SENDING", a);
-        xhr.send(a);
+        assn.clientData = btoa(String.fromCharCode.apply(null, new Uint8Array(assn.clientData)));
+        assn.authenticatorData = btoa(String.fromCharCode.apply(null, new Uint8Array(assn.authenticatorData)));
+        assn.signature = btoa(String.fromCharCode.apply(null, new Uint8Array(assn.signature)));
+
+        var resp = JSON.stringify(assn);
+        // convert ArrayBuffers to base64 encoded strings
+        console.log("SENDING", resp);
+        xhr.send(resp);
         xhr.onload = function() {
             console.log("XMLHttpRequest.onload got response for login:");
             if (xhr.status !== 200) {
@@ -203,25 +209,26 @@ function fidoLogin() {
     getLoginChallenge()
         .then(function(challenge) {
             console.log("Doing getAssertino");
-            // return webauthn.getAssertion(challenge, {}, whitelist, {})
+            return webauthn.getAssertion(challenge, 300, whitelist, {});
 
-            // dummy assertion
-            var assn = {
-                credential: {
-                    type: 'ScopedCred',
-                    id: '8DD7414D-EE43-474C-A05D-FDDB828B663B'
-                },
-                clientData: 'ew0KCSJjaGFsbGVuZ2UiIDogImFiYzEyM2RlZjQ1NiINCn0A',
-                authenticatorData: 'AQAAAAA',
-                signature: 'g22nh1Ww-qZAysuizkugZGmEisax3dtoUNzIl2LWOSARzeZxm_-nQoHfKyo8b8_XnxXwuLlW8RXBLAN38D3V2PBugPRloVzE1gn4Vl7Ro124GqPyURNllvNkD3EAl64bHPK-EVIOmI8zk0QK_ZoqfAKY_RfMLNObSn47H_hdA-YZUGEkWtcyUgC65H9xfhFWOQdg-r_pHY5_TxgdSNR8itkBb2xZGKagFnGUtdmOSRROVwK9AalJwsJD1W4lF5_4Jfumsb1YJ6yQwrPhJuYNCCeVHXIahXDUKTdTtWQs0MTj7kGi1j-_lkNpl7rEnSV4wqw8K5SEcHM-mEBYX-fMDw'
-            };
-            console.log("getAssertion done, got:");
-            console.log(assn);
-            assn.username = document.getElementById("username").value; // TODO: check first
-            return assn;
+            // // dummy assertion
+            // var assn = {
+            //     credential: {
+            //         type: 'ScopedCred',
+            //         id: '8DD7414D-EE43-474C-A05D-FDDB828B663B'
+            //     },
+            //     clientData: 'ew0KCSJjaGFsbGVuZ2UiIDogImFiYzEyM2RlZjQ1NiINCn0A',
+            //     authenticatorData: 'AQAAAAA',
+            //     signature: 'g22nh1Ww-qZAysuizkugZGmEisax3dtoUNzIl2LWOSARzeZxm_-nQoHfKyo8b8_XnxXwuLlW8RXBLAN38D3V2PBugPRloVzE1gn4Vl7Ro124GqPyURNllvNkD3EAl64bHPK-EVIOmI8zk0QK_ZoqfAKY_RfMLNObSn47H_hdA-YZUGEkWtcyUgC65H9xfhFWOQdg-r_pHY5_TxgdSNR8itkBb2xZGKagFnGUtdmOSRROVwK9AalJwsJD1W4lF5_4Jfumsb1YJ6yQwrPhJuYNCCeVHXIahXDUKTdTtWQs0MTj7kGi1j-_lkNpl7rEnSV4wqw8K5SEcHM-mEBYX-fMDw'
+            // };
+            // console.log("getAssertion done, got:");
+            // console.log(assn);
+            // return assn;
         })
         .then(function(assn) {
             console.log("Doing sendLoginResponse");
+            console.log("Assn:", assn);
+            assn.username = document.getElementById("username").value; // TODO: check first
             return sendLoginResponse(assn);
         })
         .catch(function(err) {
